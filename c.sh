21f2148dac76dc3c7f442894825d516305607ec3
kira <93389526+L0rdK1r422@users.noreply.github.com>
2021-12-31 23:39:36 +0800

    Update c.sh

--- c.sh
+++ c.sh
@@ -1,17 +1,17 @@
 # 31.12.2021
 # !/bin/bash
 # Take user Input
-echo "Masukkan 2 nombor : "
+echo "Enter 2 Number : "
 
 read a
 read b
 
 # Input type of operation 
 echo  "Enter Choice : "
-echo "1. Tambah"
-echo "2. Tolak"
-echo "3. Darab"
-echo "4. Bahagi"
+echo "1. addition(penambahan)"
+echo "2. subtraction(penolakan)"
+echo "3. multiplication(pendaraban)"
+echo "4. division(pembahagian)"
 read ch
 # Switch Case to perform
 # calculator operation
@@ -27,5 +27,6 @@ case $ch in
 ;;
 esac
 
+# Result
 echo Result : $res
 
